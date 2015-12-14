2479b2481af363ce5ed4d79ed767a8c159e7df5c
mondoman712 <oommmmmmmo@googlemail.com>
2015-12-14 16:20:22 +0000

    reads from file

--- te.c
+++ te.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#define MAXBUFFSIZE 128
 
 int main(int argc, char *argv[])
 {
-	printf("%s\n", argv[1]);
-	return 0;
+	FILE *fp;
+	char buffer[MAXBUFFSIZE];
+
+	fp = fopen(argv[1], "r");
+	if (fp == NULL) {
+		fprintf(stderr, "Failed to open %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+
+	while (fgets(buffer, MAXBUFFSIZE, fp)) {
+		printf("%s", buffer);
+	}
+
+	exit(EXIT_SUCCESS);
 }
